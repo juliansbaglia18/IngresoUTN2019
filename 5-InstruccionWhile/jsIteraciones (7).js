e93f34f0b42e21d33439e01c9d3bc7bf67926c6b
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2019-02-27 10:17:06 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,9 +3,18 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta;
+	var bel;
+	
+do {
 
+	bel = parseInt(prompt("ingrese un numero:"));
 
+	acumulador = bel + acumulador;
+	contador++;
+	alert(acumulador);
+	respuesta = confirm("desea seguir ingresando numeros?");
+} while(respuesta==true);
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
 
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,18 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
-
+do{
+	numero = parseInt(prompt ("ingrese un numero:"));
+	while(numero < 0){
+		negativo = numero * negativo;
+	}
+	while (numero >= 0){
+		positivo = positivo + numero;
+	}
+	respuesta = prompt("desea continuar?");
+}while(respuesta == "si" || respuesta == "SI" || respuesta == "Si");
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
